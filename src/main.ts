8d078a3b03f25c9312032ad1881436373182aa37
ali01129 <chaudharyalinawazz@gmail.com>
2025-12-06 02:05:10 +0500

    Added swagger in project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Recipt Backend API')
+    .setDescription('API documentation for Recipt Backend')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Server Started Successfully: http://localhost:${process.env.PORT ?? 3000}`);
+  console.log(`Swagger documentation: http://localhost:${process.env.PORT ?? 3000}/api`);
 }
 bootstrap();
